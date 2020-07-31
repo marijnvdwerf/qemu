1ac57b5e649943331d6d8b41fed14b20e1671d8a
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2020-07-31 13:37:03 +0200

    Move device code to soc code

---
+++ hw/arm/da1469x/soc.h
@@ -0,0 +1,8 @@
+#ifndef HW_ARM_STM32F205_SOC_H
+#define HW_ARM_STM32F205_SOC_H
+
+#define TYPE_DA1469X_SOC "da1469x-soc"
+#define DA1469X_SOC(obj)                                       \
+    OBJECT_CHECK(DA2469xState, obj, TYPE_DA1469X_SOC)
+
+#endif
